49bd126dd80e3d601cd04468e87d48118bd4faf1
anastasiya kostenko <kostenkonastya@gmail.com>
2022-08-17 15:15:39 +0200

    done without style

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new', as: :new_review
+  post 'restaurants/:restaurant_id/reviews', to: 'reviews#create', as: :restaurant_reviews
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'restaurants', to: 'restaurants#index', as: :restaurants
+  get 'restaurants/new', to: 'restaurants#new', as: :new
+  post 'restaurants', to: 'restaurants#create'
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
 end
